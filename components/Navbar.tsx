b042ab7ef3da02e920607e44e839ad03adf3cc98
Stepan72 <st.nim@list.ru>
2024-04-12 06:55:21 +0500

    Added constants and assets v0.4

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
+      <Link href="/">
+        <Image src="/hilink-logo.svg" alt="TravelLogo" width={74} height={29} />
+      </Link>
+      <ul className="hidden h-full gap-12 lg:flex"></ul>
+    </nav>
+  );
 };
 
 export default Navbar;
