13b96aa35674b0639844cf3c5fdb677781721aa3
Stepan72 <st.nim@list.ru>
2024-04-16 07:13:08 +0500

    Features started v1.1

--- components/Button.tsx
+++ components/Button.tsx
@@ -12,10 +12,12 @@ const Button = ({ title, type, icon, variant }: ButtonProps) => {
   return (
     <button
       type={type}
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      className={`flexCenter gap-3  rounded-full border ${variant}`}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
-      <label className="bold-16 whitespace-nowrap">{title}</label>
+      <label className="bold-16 whitespace-nowrap cursor-pointer">
+        {title}
+      </label>
     </button>
   );
 };
---
+++ components/FeatureItem.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FeatureItem = () => {
+  return <div>FeatureItem</div>;
+};
+
+export default FeatureItem;
