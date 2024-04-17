a5983976310ef7bca762a1f815f7c1027ed4e934
Stepan72 <st.nim@list.ru>
2024-04-17 05:37:34 +0500

    Features finished v1.2

--- components/FeatureItem.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FeatureItem = () => {
-  return <div>FeatureItem</div>;
-};
-
-export default FeatureItem;
--- components/Features.tsx
+++ components/Features.tsx
@@ -1,12 +1,29 @@
 import { FEATURES } from "@/constants";
 import Image from "next/image";
 import React from "react";
-import FeatureItem from "./FeatureItem";
+
+interface FeatureItemProps {
+  title: string;
+  icon: string;
+  description: string;
+}
+
+const FeatureItem = ({ title, icon, description }: FeatureItemProps) => (
+  <li className="flex w-full flex-1 flex-col items-start">
+    <div className="rounded-full p-4 lg:p-7 bg-green-50">
+      <Image src={icon} alt="map" width={28} height={28} />
+    </div>
+    <h2 className="bold-20 lg:bold-32 capitalize mt-5">{title}</h2>
+    <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none">
+      {description}
+    </p>
+  </li>
+);
 
 const Features = () => {
   return (
-    <section className="border-2 border-red-500 flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
-      <div className="max-container padding-container relative w-full justify-end">
+    <section className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
+      <div className="flex max-container padding-container relative w-full justify-end">
         <div className="flex flex-1 lg:min-h-[900px]">
           <Image
             src="/phone.png"
@@ -27,10 +44,14 @@ const Features = () => {
             />
             <h2 className="bold-40 lg:bold-64">Our Features</h2>
           </div>
-          <ul>
+          <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mt-20 lg:gap-20">
             {FEATURES.map((feature) => (
-              <FeatureItem />
-              // 1:26:22
+              <FeatureItem
+                key={feature.title}
+                title={feature.title}
+                icon={feature.icon}
+                description={feature.description}
+              />
             ))}
           </ul>
         </div>
