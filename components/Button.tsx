24dcf2922974a53bd2352e2a6a99b43159011b8a
Stepan72 <st.nim@list.ru>
2024-04-17 06:02:58 +0500

    GetApp finished v1.3

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,14 +5,17 @@ interface ButtonProps {
   title: string;
   type: "button" | "reset" | "submit";
   icon?: string;
-  variant: "btn_dark_green" | "btn_green" | "btn_white_text";
+  variant: string;
+  full?: boolean;
 }
 
-const Button = ({ title, type, icon, variant }: ButtonProps) => {
+const Button = ({ title, type, icon, variant, full }: ButtonProps) => {
   return (
     <button
       type={type}
-      className={`flexCenter gap-3  rounded-full border ${variant}`}
+      className={`flexCenter h-12 gap-3  rounded-full border ${variant} ${
+        full && "w-full"
+      }`}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
       <label className="bold-16 whitespace-nowrap cursor-pointer">
