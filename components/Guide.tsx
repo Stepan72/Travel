9f28061e0b7ee3b0a5b2d0da1dd7d967569af9c3
Stepan72 <st.nim@list.ru>
2024-09-18 17:34:43 +0500

    changed destination in Guide v1.41

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -49,7 +49,7 @@ const Guide = () => {
             <div className="flex w-full flex-col">
               <p className="regular-16 text-gray-20">Start track</p>
               <h4 className="bold-20 mt-2 whitespace-nowrap">
-                Aguas Calientes
+                Wonorejo Pasuruan
               </h4>
             </div>
           </div>
